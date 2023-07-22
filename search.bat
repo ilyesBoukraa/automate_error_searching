882f80a3b0d0682c7117ea4930028ecb43a8bc75
ilyesBoukraa <boukraailyesali@gmail.com>
2023-07-22 22:47:39 +0200

    Update search.bat

--- search.bat
+++ search.bat
@@ -1,2 +1,2 @@
 @echo off
-@start "" "C:\Users\boukr\AppData\Local\Microsoft\WindowsApps\pythonw.exe" "E:\practise_batch\search_google.py" %*
+@start "" "<your_python_path>\pythonw.exe" "<this_directory_path>\search_google.py" %*
